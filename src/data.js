11d294fc8b07a5519c283f4a965d97ec6a9b37a6
sudomarcus <kaimun.create@gmail.com>
2026-01-18 14:14:12 -0800

    Fix asset paths and move to public

--- src/data.js
+++ src/data.js
@@ -3,49 +3,49 @@ export const tracks = [
         title: "love u girl",
         url: "https://soundcloud.com/vjvidly-746351807/love-u-girl",
         artist: "vjvidly",
-        image: "/src/assets/love_u_girl.jpg",
-        audio: "/src/assets/audio/love-u-girl.mp3"
+        image: "/assets/love_u_girl.jpg",
+        audio: "/assets/audio/love-u-girl.mp3"
     },
     {
         title: "love me",
         url: "https://soundcloud.com/vjvidly-746351807/love-me",
         artist: "vjvidly",
-        image: "/src/assets/love_me.png",
-        audio: "/src/assets/audio/love-me.mp3"
+        image: "/assets/love_me.png",
+        audio: "/assets/audio/love-me.mp3"
     },
     {
         title: "falling",
         url: "https://soundcloud.com/vjvidly-746351807/falling",
         artist: "vjvidly",
-        image: "/src/assets/falling.jpg",
-        audio: "/src/assets/audio/falling.mp3"
+        image: "/assets/falling.jpg",
+        audio: "/assets/audio/falling.mp3"
     },
     {
         title: "serafin",
         url: "https://soundcloud.com/vjvidly-746351807/serafin",
         artist: "vjvidly x gemstonebullets",
-        image: "/src/assets/serafin.jpg",
-        audio: "/src/assets/audio/serafin.mp3"
+        image: "/assets/serafin.jpg",
+        audio: "/assets/audio/serafin.mp3"
     },
     {
         title: "Project 46",
         url: "https://soundcloud.com/vjvidly-746351807/project-46-vjvidlyy-mp3",
         artist: "vjvidly",
-        image: "/src/assets/project_46.jpg",
-        audio: "/src/assets/audio/project-46.mp3"
+        image: "/assets/project_46.jpg",
+        audio: "/assets/audio/project-46.mp3"
     },
     {
         title: "Project 48",
         url: "https://soundcloud.com/vjvidly-746351807/project-48",
         artist: "vjvidly",
-        image: "/src/assets/project_48.jpg",
-        audio: "/src/assets/audio/project-48.mp3"
+        image: "/assets/project_48.jpg",
+        audio: "/assets/audio/project-48.mp3"
     },
     {
         title: "NO CREED",
         url: "https://soundcloud.com/vjvidly-746351807/no-creed",
         artist: "vjvidly",
-        image: "/src/assets/no_creed.jpg",
-        audio: "/src/assets/audio/no-creed.mp3"
+        image: "/assets/no_creed.jpg",
+        audio: "/assets/audio/no-creed.mp3"
     }
 ];
